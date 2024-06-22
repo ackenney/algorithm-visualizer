e176772519d3ce2d37d97c68696a8496b87bd99f
ackenney <ackenney@gmail.com>
2024-06-22 14:28:03 -0400

    create quick sort function

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import random
 
-def main():
-    sortedArray = [1,2,3,4,5,6,7,8,9,10]
-    unsortedArray = [9, 2, 3, 5, 7, 10, 8, 4, 6, 1]
-    #random.shuffle(sortedArray)
-    #print(unsortedArray)    
-
-
-if __name__== "__main__": 
-    main() 
-    
     
 #FIXME
 def mergeSort():
     pass
 
 
-#FIXME
-def quickSort():
-    pass
+# quick sort functions
+def quickSort(unsortedArray):
+    
+    
+    isGreater =[]
+    isLower = []
+    
+    length = len(unsortedArray)
+    
+    if length <= 1:
+        return unsortedArray;
+    else:
+        pivot = unsortedArray.pop()
+    
+    for index in unsortedArray:
+        if index > pivot:
+             isGreater.append(index)
+        else:
+            isLower.append(index)
+            
+    newArray = quickSort(isLower) + [pivot] + quickSort(isGreater)
+
+    return newArray
+
+
 
 
 #FIXME
@@ -34,3 +46,13 @@ def insertionSort():
 #FIXME
 def bubbleSort():
     pass
+
+
+
+if __name__== "__main__": 
+    sortedArray = [1,2,3,4,5,6,7,8,9,10]
+    unsortedArray = [9, 2, 3, 5, 7, 10, 8, 4, 6, 1]
+    #random.shuffle(sortedArray)
+    
+    print(unsortedArray)    
+    print(quickSort(unsortedArray))
