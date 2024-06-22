12782c11c7fd7c461aa0b4935aef1d12ddd270b7
ackenney <ackenney@gmail.com>
2024-06-22 12:46:55 -0400

    make algorithm templates

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
+import random
+
 def main():
+    sortedArray = [1,2,3,4,5,6,7,8,9,10]
+    unsortedArray = [9, 2, 3, 5, 7, 10, 8, 4, 6, 1]
+    #random.shuffle(sortedArray)
+    #print(unsortedArray)    
+
+
+if __name__== "__main__": 
+    main() 
+    
+    
+#FIXME
+def mergeSort():
+    pass
+
+
+#FIXME
+def quickSort():
+    pass
+
+
+#FIXME
+def heapSort():
+    pass
+
+
+#FIXME
+def insertionSort():
+    pass
+
+
+#FIXME
+def bubbleSort():
     pass
