7d316d72ede5492b56642c997fc2133dbd28b33d
ackenney <ackenney@gmail.com>
2024-07-03 13:00:01 -0400

    Complete selection sort algorithm

--- SortingAlgorithms/selectionSort.py
+++ SortingAlgorithms/selectionSort.py
@@ -1,22 +1,21 @@
-# selction sort function
+# Implementation of Selction Sort function
 
 from visualizer import displayArray
 
-
 def selectionSort(displayInformation):
-    unsortedArray = displayInformation.array
-    indexLength = range(0,len(unsortedArray)-1)
+    array = displayInformation.array
+    indexLength = range(0,len(array)-1)
     
     for i in indexLength:
         minimumValue = i
         
-        for j in range(i+1,len(unsortedArray)):
-            if unsortedArray[j] < unsortedArray[minimumValue]:
-                minimumValue = j
-                displayArray(displayInformation, {j: displayInformation.RED, i: displayInformation.GREEN}, True)
+        for j in range(i+1,len(array)):
+            if array[j] < array[minimumValue]:
+                minimumValue = j # Set new minimum value element
+                
+                # Display array at current state
+                displayArray(displayInformation, {j: displayInformation.RED, i-1: displayInformation.GREEN}, True)
                 yield 
-        if minimumValue != i:
-            unsortedArray[minimumValue], unsortedArray[i] = unsortedArray[i], unsortedArray[minimumValue]
-            #displayArray(displayInformation, {i: displayInformation.GREEN, j: displayInformation.RED}, True)
-            #yield True
-    return unsortedArray
+        if minimumValue != i: # Swap elements if not minimum value
+            array[minimumValue], array[i] = array[i], array[minimumValue]
+    return array # Return sorted array
