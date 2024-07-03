99f3a42c29e2956e44c6587dfbd1dd7e2b08f7dc
ackenney <ackenney@gmail.com>
2024-07-03 12:14:26 -0400

    update bubble sort

--- SortingAlgorithms/bubbleSort.py
+++ SortingAlgorithms/bubbleSort.py
@@ -1,20 +1,25 @@
-# bubble sort function
+# Bubble sort function
+
 from visualizer import displayArray
 
 
 def bubbleSort(displayInformation):
-	array = displayInformation.array
+    
+    # Start with unsorted array
+	array = displayInformation.array 
 
 	for i in range(len(array) - 1):
 		for j in range(len(array) - 1 - i):
-			num1 = array[j]
-			num2 = array[j + 1]
-   
-			if (num1 > num2):
+
+			if (array[j] > array[j + 1]):
+				# Swap positions if element found is greater
 				array[j], array[j + 1] = array[j + 1], array[j]
+    
+				# Display array
 				displayArray(displayInformation, {j: displayInformation.GREEN, j + 1: displayInformation.RED}, True)
 				yield True
-
-	return array
+	
+ 	# Return sorted array
+	return array 
 
 
